41ca12d7816f1bbc80d3146b39904bcdbf130499
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-04-21 19:59:36 +0200

    modification rapide

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // function color() {
-// 	document.getElementsByClassName('cercle').style.backgroundColor = "red";
+//  document.getElementsByClassName('cercle').style.backgroundColor = "red";
 // }
 
 var int = document.getElementById('intro');
 
 
 function epaule() {
- 	var ep = document.getElementById('tepaule');
- 	var couleur = document.getElementById('epaule');
+    var ep = document.getElementById('tepaule');
+    var couleur = document.getElementById('epaule');
       
     if(ep.style.display == 'block') {
-    	ep.style.display = 'none';
-    	couleur.style.backgroundColor = 'black';
-    	couleur.style.opacity = "0.7";
-    	int.style.display = "block";
-   	} else {
+        ep.style.display = 'none';
+        couleur.style.backgroundColor = 'black';
+        couleur.style.opacity = "0.7";
+        int.style.display = "block";
+    } else {
         ep.style.display = 'block';
         couleur.style.backgroundColor = 'red';
         couleur.style.opacity = "0.7"
@@ -25,87 +25,87 @@ function epaule() {
 
 
 function coude(){
- 	var cd = document.getElementById('tcoude');
+    var cd = document.getElementById('tcoude');
     var couleurC = document.getElementById('coude');
 
     if(cd.style.display == 'block') {
         cd.style.display = 'none';
         couleurC.style.backgroundColor = 'black';
-    	couleurC.style.opacity = "0.7";
-    	int.style.display = "block";
+        couleurC.style.opacity = "0.7";
+        int.style.display = "block";
     } else {
         cd.style.display = 'block';
         couleurC.style.backgroundColor = 'red';
         int.style.display = "none";
-    	couleurC.style.opacity = "0.7" 
+        couleurC.style.opacity = "0.7" 
     }
 }
 
 function poignet() {
- 	var tp = document.getElementById('tpoignet');
- 	var couleurP = document.getElementById('poignet');
+    var tp = document.getElementById('tpoignet');
+    var couleurP = document.getElementById('poignet');
 
     if (tp.style.display == 'block') {
         tp.style.display = 'none';
         couleurP.style.backgroundColor = 'black';
-    	couleurP.style.opacity = "0.7";
-    	int.style.display = "block";
+        couleurP.style.opacity = "0.7";
+        int.style.display = "block";
     } else {
         tp.style.display = 'block';
         couleurP.style.backgroundColor = 'red';
-    	couleurP.style.opacity = "0.7"
-    	int.style.display = "none";
+        couleurP.style.opacity = "0.7"
+        int.style.display = "none";
     }
 }
 
 
 function hanche() {
- 	var ha = document.getElementById('thanche');
- 	var couleurH = document.getElementById('hanche');
+    var ha = document.getElementById('thanche');
+    var couleurH = document.getElementById('hanche');
  
     if(ha.style.display == 'block') {
         ha.style.display = 'none';
         couleurH.style.backgroundColor = 'black';
-    	couleurH.style.opacity = "0.7";
-    	int.style.display = "block";
+        couleurH.style.opacity = "0.7";
+        int.style.display = "block";
     } else {
         ha.style.display = 'block';
         couleurH.style.backgroundColor = 'red';
-    	couleurH.style.opacity = "0.7"
-    	int.style.display = "none";
+        couleurH.style.opacity = "0.7"
+        int.style.display = "none";
     }
 }
 
 function genoux() {
- 	var ge = document.getElementById('tgenoux');
-  	var couleurG = document.getElementById('genoux');
+    var ge = document.getElementById('tgenoux');
+    var couleurG = document.getElementById('genoux');
  
     if(ge.style.display == 'block') {
         ge.style.display = 'none';
         couleurG.style.backgroundColor = 'black';
-    	couleurG.style.opacity = "0.7"
-    	int.style.display = "block";
+        couleurG.style.opacity = "0.7"
+        int.style.display = "block";
     } else {
         ge.style.display = 'block';
         couleurG.style.backgroundColor = 'red';
-    	couleurG.style.opacity = "0.7";
-    	int.style.display = "none";
-    	}
-	}
+        couleurG.style.opacity = "0.7";
+        int.style.display = "none";
+        }
+    }
 
 function cheville() {
- 	var tc = document.getElementById('tcheville');
- 	var couleurCh = document.getElementById('cheville');
+    var tc = document.getElementById('tcheville');
+    var couleurCh = document.getElementById('cheville');
 
     if(tc.style.display == 'block') {
         tc.style.display = 'none';
         couleurCh.style.backgroundColor = 'black';
-    	couleurCh.style.opacity = "0.7";
-    	int.style.display = "block";
+        couleurCh.style.opacity = "0.7";
+        int.style.display = "block";
     } else {
         tc.style.display = 'block';
         couleurCh.style.backgroundColor = 'red';
-    	couleurCh.style.opacity = "0.7"
-    	int.style.display = "none";
+        couleurCh.style.opacity = "0.7"
+        int.style.display = "none";
      }
 }
