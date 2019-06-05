1a2738338854f5c59427efddac92bd3e6c78b08f
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-06-05 15:35:03 +0200

    rework of the JS entierly

--- index.js
+++ index.js
@@ -1,111 +1,28 @@
-// function color() {
-//  document.getElementsByClassName('cercle').style.backgroundColor = "red";
-// }
+// Création d'une variable cercle qui va regrouper TOUTES les div avec class = cercle
+let cercle = document.querySelectorAll('.cercle');
 
-var int = document.getElementById('intro');
-
-
-function epaule() {
-    var ep = document.getElementById('tepaule');
-    var couleur = document.getElementById('epaule');
-      
-    if(ep.style.display == 'block') {
-        ep.style.display = 'none';
-        couleur.style.backgroundColor = 'black';
-        couleur.style.opacity = "0.7";
-        int.style.display = "block";
-    } else {
-        ep.style.display = 'block';
-        couleur.style.backgroundColor = 'red';
-        couleur.style.opacity = "0.7"
-        int.style.display = "none";
-
-    }
-}
-
-
-function coude(){
-    var cd = document.getElementById('tcoude');
-    var couleurC = document.getElementById('coude');
-
-    if(cd.style.display == 'block') {
-        cd.style.display = 'none';
-        couleurC.style.backgroundColor = 'black';
-        couleurC.style.opacity = "0.7";
-        int.style.display = "block";
-    } else {
-        cd.style.display = 'block';
-        couleurC.style.backgroundColor = 'red';
-        int.style.display = "none";
-        couleurC.style.opacity = "0.7" 
-    }
-}
-
-function poignet() {
-    var tp = document.getElementById('tpoignet');
-    var couleurP = document.getElementById('poignet');
-
-    if (tp.style.display == 'block') {
-        tp.style.display = 'none';
-        couleurP.style.backgroundColor = 'black';
-        couleurP.style.opacity = "0.7";
-        int.style.display = "block";
-    } else {
-        tp.style.display = 'block';
-        couleurP.style.backgroundColor = 'red';
-        couleurP.style.opacity = "0.7"
-        int.style.display = "none";
-    }
-}
-
-
-function hanche() {
-    var ha = document.getElementById('thanche');
-    var couleurH = document.getElementById('hanche');
- 
-    if(ha.style.display == 'block') {
-        ha.style.display = 'none';
-        couleurH.style.backgroundColor = 'black';
-        couleurH.style.opacity = "0.7";
-        int.style.display = "block";
-    } else {
-        ha.style.display = 'block';
-        couleurH.style.backgroundColor = 'red';
-        couleurH.style.opacity = "0.7"
-        int.style.display = "none";
-    }
-}
-
-function genoux() {
-    var ge = document.getElementById('tgenoux');
-    var couleurG = document.getElementById('genoux');
- 
-    if(ge.style.display == 'block') {
-        ge.style.display = 'none';
-        couleurG.style.backgroundColor = 'black';
-        couleurG.style.opacity = "0.7"
-        int.style.display = "block";
-    } else {
-        ge.style.display = 'block';
-        couleurG.style.backgroundColor = 'red';
-        couleurG.style.opacity = "0.7";
-        int.style.display = "none";
+// On fais une boucle pour parcourir la liste cercle 
+for (let i = 0; i < cercle.length; i++) {
+    // On ajoute une fonction au clique pour CHAQUE elements de cercle
+    cercle[i].onclick = function() {
+        // Création d'une variable qui va stocker l'id de la division sur lequel le click sera effectuer 
+        let divCercle = this.id;
+        // Contrainte que si l'element sur lequel on clique ne contiens pas la class active ->
+        if (!this.classList.contains("active")) {
+            // Pour chaque element de cercle[j]
+            for (let j = 0; j < cercle.length; j++) {
+                // On enleve la class active
+                cercle[j].classList.remove("active");
+                // Et on met le texte en display none 
+                document.getElementById("texte_" + cercle[j].id).style.display = "none";
+            }
+            // On ajoute la classe Active
+            this.classList.add("active");
+            // On va cacher le texte d'introduction
+            document.getElementById("intro").style.display = "none";
+            // On affiche le texte correspondant à l'id du clique
+            document.getElementById("texte_" + divCercle).style.display = "block";
         }
     }
 
-function cheville() {
-    var tc = document.getElementById('tcheville');
-    var couleurCh = document.getElementById('cheville');
-
-    if(tc.style.display == 'block') {
-        tc.style.display = 'none';
-        couleurCh.style.backgroundColor = 'black';
-        couleurCh.style.opacity = "0.7";
-        int.style.display = "block";
-    } else {
-        tc.style.display = 'block';
-        couleurCh.style.backgroundColor = 'red';
-        couleurCh.style.opacity = "0.7"
-        int.style.display = "none";
-     }
 }
